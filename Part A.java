2165cb53af8942d4f3aa5a7f9658998af7b79a77
mehtaAkash <akashkumar979868@gmail.com>
2025-10-05 21:06:14 +0530

    Create Part A.java

---
+++ Part A.java
@@ -0,0 +1,13 @@
+public class SumOfIntegers {
+    public static void main(String[] args) {
+
+        Integer num1 = 10;  
+        Integer num2 = 20;
+        Integer num3 = 30;
+
+
+        int sum = num1 + num2 + num3;  
+
+        System.out.println("Sum of Integers = " + sum);
+    }
+}
